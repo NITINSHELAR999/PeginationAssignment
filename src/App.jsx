82520250d80f6a00d52bbf69681099df7dac5141
NITEEN <your-email-address>
2022-03-25 19:19:07 +0530

    pegination

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Pegination from './Components/Pegination';
+function App() {
+  return (
+    <main>
+    <Pegination/>
+    </main>
+  );
+}
+
+export default App;
